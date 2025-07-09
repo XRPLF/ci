e1e488e938debd4c84908464a84a9b7b9297edbf
Bart Thomee <11445373+bthomee@users.noreply.github.com>
2025-07-09 12:33:53 -0400

    Run test within Dockerfile

--- test/run.sh
+++ test/run.sh
@@ -2,15 +2,11 @@
 
 set -ex
 
-# Copy the test code, as we do not want to modify the directory.
-cp -r /tmp test
-cd test
-
-# Install Conan dependencies and build the project.
+# Install Conan dependencies, configure the project, and build the executable.
 conan install . --output-folder=build --build=missing
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 
-# Run the compressor executable.
-./compressor
+# Run the executable.
+./example
